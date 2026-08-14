676fcf9052885042158417009733a3fe7921ff18
Pratham <prathams1306@gmail.com>
2026-08-13 20:42:17 -0400

    .

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import requests
+import time
 
+results = []
+
+for row in routes:
+    result = get_driving_data(
+        row["origin_latitude"],
+        row["origin_longitude"],
+        row["destination_latitude"],
+        row["destination_longitude"]
+    )
+
+    results.append({
+        "team_id": row["team_id"],
+        "venue_id": row["venue_id"],
+        "driving_distance_km": result["distance_km"],
+        "driving_time_hours": result["time_hours"]
+    })
+
+    time.sleep(0.2)
+    
 def get_driving_data(lat1, lon1, lat2, lon2):
     url = (
         "https://router.project-osrm.org/route/v1/driving/"
